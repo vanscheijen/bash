d062fd341dc2eae5411292234703ecf46a0e24d8
vanScheijen <github@vanscheijen.nl>
2021-05-07 16:02:40 +0200

    Moved most into the comma_bash_profile

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-bash
-====
+# bash script collection
 
 Tools and utilities written in or for bash.
+
+Most scripts have been made into functions for my Comma .bash_profile, see:
+https://github.com/vanscheijen/comma_bash_profile
+
--- proof_of_work.sh
+++ proof_of_work.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
-#
-# (ↄ)2014 vanScheijen
-#---------------------
-# Simple and inefficient proof of work tool, using any hash/salt/proof.
-#
+## Bash Proof of Work
+## ©2012-2021 vanScheijen
+## Usage of the works is permitted provided that this instrument is retained with the works, so that any entity that uses the works is notified of this instrument.
+## DISCLAIMER: THE WORKS ARE WITHOUT WARRANTY.
+## ----
+## Simple and *inefficient* proof of work implementation in bash. Uses any hash binary, salt, or proof.
+## https://en.wikipedia.org/wiki/Proof_of_work
 
-hashbin=sha1sum
-time_start=`date +%s`
+#hashbin=md5sum
+#hashbin=sha1sum
+hashbin=sha256sum
+
 salt="$1"
-proof="${2-^00}"
+proof="${2:-^00}"
 
 if [[ -z $salt ]]; then
-	echo "Usage: $0 <salt> [proof]"
-	exit 1
+    echo "Usage: $0 <salt> [proof]"
+    exit 1
 fi
 
 work=0
 echo "Searching"
-while true; do
-   hash=`echo "${salt}${work}" | $hashbin`
-   echo -n "."
+time_start=`date +%s`
+while :; do
+    hash=`echo -n "${salt}${work}" | $hashbin`
+    echo -n "."
 
-   if [[ "$hash" =~ $proof ]]; then
-		time_end=`date +%s`
-		time_spent=$(($time_end - $time_start))
-		echo "found!"
-		echo "hashbin: $hashbin"
-		echo "proof: $proof"
-		echo "salt: $salt"
-		echo "work: $work"
-		echo "hash: $hash"
-		echo "time spend: ${time_spent}s"
-		echo "speed: $(($work / $time_spent)) hash/s"
-		exit 0
-   fi
+    if [[ "$hash" =~ $proof ]]; then
+        time_end=`date +%s`
+        time_spent=$((time_end - time_start))
+        echo "found!"
+        echo "hashbin: $hashbin"
+        echo "proof: $proof"
+        echo "salt: $salt"
+        echo "work: $work"
+        echo "hash: $hash"
+        echo "time spend: ${time_spent}s"
+        echo "speed: $(($work / $time_spent)) hash/s"
+        exit 0
+    fi
 
-   (( work++ ))
+    (( work++ ))
 done
 
